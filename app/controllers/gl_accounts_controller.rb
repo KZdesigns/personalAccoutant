bb577127ee11065e74bc011a3a1a853ecde436a9
Kyle Zimmerman <kzdesignsios@gmail.com>
2022-04-23 01:00:36 -0700

    full crud operation on gl Accounts working

--- app/controllers/gl_accounts_controller.rb
+++ app/controllers/gl_accounts_controller.rb
@@ -17,6 +17,27 @@ class GlAccountsController < ApplicationController
         end
     end
 
+    def show
+        @gl_account = GlAccount.find(params[:id])
+    end
+
+    def update
+        @gl_account = GlAccount.find(params[:id])
+
+        if @gl_account.update(account_params)
+            redirect_to action: "index"
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @gl_account = GlAccount.find(params[:id])
+        @gl_account.destroy
+    
+        redirect_to gl_accounts_path, status: :see_other
+      end
+    
     private 
     def account_params
         params.require(:gl_account).permit(:name, :number, :gl_type)
