04aac8130eb754fa97839d54d1b84ae1eda3346a
Kyle Zimmerman <kzdesignsios@gmail.com>
2022-08-18 09:12:40 -0700

    added notes select input and update button, TODO: finish transaction onUpdateHandler

--- personal-accountant/src/components/Transactions.js
+++ personal-accountant/src/components/Transactions.js
@@ -12,18 +12,32 @@ const Transactions = (props) => {
     props.setFile("delete");
   };
 
+  const onUpdateHandler = async (even) => {
+    // TODO: write function for updating transactions
+    // PUT    /api/v1/transactions/:id(.:format)
+  };
+
   return (
     <div>
       <h1>This Transaction are from the API</h1>
       {props.transactions.map((txn) => {
         return (
           <div key={txn.id}>
-            <span>{txn.date}</span>
-            <span>{txn.amount}</span>
-            <span>{txn.description}</span>
-            <button onClick={onDeleteHandler} value={txn.id}>
-              Delete
-            </button>
+            <form>
+              <input type="text" defaultValue={txn.date}></input>
+              <input type="text" defaultValue={txn.amount}></input>
+              <input type="text" defaultValue={txn.description}></input>
+              <select>
+                <option value="none" defaultChecked></option>
+                <option value="need to request">need to request</option>
+                <option value="need to request">requested</option>
+                <option value="need to request">paid</option>
+              </select>
+              <button onClick={onDeleteHandler} value={txn.id}>
+                Delete
+              </button>
+              <button onClick={onUpdateHandler}>Update Txn</button>
+            </form>
           </div>
         );
       })}
