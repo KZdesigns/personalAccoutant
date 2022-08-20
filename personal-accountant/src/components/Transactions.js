21346b347f94690650eab919a2ce7032f29ac9ed
Kyle Zimmerman <kzdesignsios@gmail.com>
2022-08-20 10:41:25 -0700

    created filter in frontend app, thinking of creating filter in backend and sending the data up to front end to show transactions that need to be requested

--- personal-accountant/src/components/Transactions.js
+++ personal-accountant/src/components/Transactions.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import TxnItem from "./TxnItem";
 import classes from "./Transaction.module.css";
 
 const Transactions = (props) => {
+  const [showFilter, setShowFilter] = useState(true);
+  const NOTES = ["Need to request", "Requested", "Paid"];
+
+  const filterTxns = props.transactions.filter((txn) => {
+    return NOTES.includes(txn.notes);
+  });
+
+  const onFilterHandler = () => {
+    setShowFilter((preState) => !preState);
+  };
+
   return (
     <React.Fragment>
       <h1>Transactions List</h1>
+      <button onClick={onFilterHandler}>Filter Transactions</button>
       <div className={classes.txnList}>
-        {props.transactions.map((txn) => (
-          <TxnItem
-            key={txn.id}
-            id={txn.id}
-            date={txn.date}
-            amount={txn.amount}
-            description={txn.description}
-            notes={txn.notes}
-            gl_account_id={txn.gl_account_id}
-            setFile={props.setFile}
-            gl_accounts={props.gl_accounts}
-            setTransactions={props.setTransactions}
-          />
-        ))}
+        {showFilter &&
+          props.transactions.map((txn) => (
+            <TxnItem
+              key={txn.id}
+              id={txn.id}
+              date={txn.date}
+              amount={txn.amount}
+              description={txn.description}
+              notes={txn.notes}
+              gl_account_id={txn.gl_account_id}
+              setFile={props.setFile}
+              gl_accounts={props.gl_accounts}
+              setTransactions={props.setTransactions}
+            />
+          ))}
+        {!showFilter &&
+          filterTxns.map((txn) => (
+            <TxnItem
+              key={txn.id}
+              id={txn.id}
+              date={txn.date}
+              amount={txn.amount}
+              description={txn.description}
+              notes={txn.notes}
+              gl_account_id={txn.gl_account_id}
+              setFile={props.setFile}
+              gl_accounts={props.gl_accounts}
+              setTransactions={props.setTransactions}
+            />
+          ))}
       </div>
     </React.Fragment>
   );
